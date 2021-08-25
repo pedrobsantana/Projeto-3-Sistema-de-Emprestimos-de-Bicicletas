aec19069f4adefca591c87b7bfca91680e088b88
Pedro Bastos Sant Ana <pedrosan84@gmail.com>
2021-08-25 16:03:20 -0300

    definição das classes

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import datetime as dt
 
 class Cliente(object):
-    pass
-
+    def __init__(self, nome, cpf, calculadoraTempo, carteira):
+        self.nome = nome
+        self.cpf = cpf
+        self.calculadoraTempo = calculadoraTempo
+        self.carteira = carteira
+        self.conta = 0.0
 
 
 class Loja(object):
-    pass
+    def __init__(self, estoque, pedido, calculadoraTempo, caixa):
+        self.estoque = estoque
+        self.pedido = pedido
+        self.calculadoraTempo = calculadoraTempo
+        self.caixa = caixa
+        
