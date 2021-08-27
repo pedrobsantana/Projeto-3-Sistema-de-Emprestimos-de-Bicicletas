447945dea3b9390a8280997f4e8ea08ad6cddf24
Pedro Bastos Sant Ana <pedrosan84@gmail.com>
2021-08-27 13:30:51 -0300

    estrutura de testes

--- test.py
+++ test.py
@@ -1,1 +1,11 @@
 import unittest
+from main import Cliente, Loja
+
+class TestaEmprestimoBikes(unittest.TestCase):
+    def setUp(self):
+        self.cliente = Cliente(2, 1, 12)
+        self.loja = Loja(100)
+
+
+if __name__ == "__main__":
+    unittest.main()
